0ca87b46d9b74d6091f2c926a364b89f9307f084
DiD3n <den190242@gmail.com>
2021-12-15 22:34:55 +0100

    Add missing ; in server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const fallback = require('express-history-api-fallback')
-const express = require('express')
+const fallback = require('express-history-api-fallback');
+const express = require('express');
 const path = require('path');
 
 const app = express();
