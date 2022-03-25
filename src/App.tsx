6976f1361f653c893c47df6f7dddeb985956366a
DiD3n <denwojtkowiak@gmail.com>
2022-03-25 14:00:28 +0100

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { AuthContextProvider } from './common/auth/AuthContext';
 import { AppRouter } from './views/AppRouter';
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: 'https://48p1r2roz4.sse.codesandbox.io',
+    cache: new InMemoryCache()
+});
 
 export const App: React.FC = () => {
 
     return (
-        <AuthContextProvider>
-            <AppRouter />
-        </AuthContextProvider>
+        <ApolloProvider client={client}>
+            <AuthContextProvider>
+                <AppRouter />
+            </AuthContextProvider>
+        </ApolloProvider>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
 
 import { App } from './App';
-
-const client = new ApolloClient({
-    uri: 'https://48p1r2roz4.sse.codesandbox.io',
-    cache: new InMemoryCache()
-});
-
 
 import './index.css';
 
 render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
+    <App />,
     document.getElementById('root')
 );
