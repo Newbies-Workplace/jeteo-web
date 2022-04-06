dd943c044a0d57d6611679852ab19170c66a9e7d
DiD3n <denwojtkowiak@gmail.com>
2022-04-06 20:23:15 +0200

    Add NavBar & Logo component

---
+++ src/components/ui/NavBar/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './NavBar.module.css';
+import {AppLogo} from "../AppLogo/AppLogo";
+
+export const NavBar: React.FC = () => {
+
+    return (
+        <div className={styles.container}>
+            <AppLogo/>
+
+            burger
+        </div>
+    );
+};
