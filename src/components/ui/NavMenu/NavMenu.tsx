c87156e93c9105f6070bea21d8b8f0080495f92d
DiD3n <denwojtkowiak@gmail.com>
2022-04-09 13:19:12 +0200

    Move NavMenu to ui

---
+++ src/components/ui/NavMenu/NavMenu.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import styles from './NavMenu.module.scss';
+import {useAuth} from "../../../contexts/auth/hooks/useAuth.hook";
+import {NavMenuItemLink} from "./NavMenuItem/NavMenuItemLink";
+import {NavMenuItemButton} from "./NavMenuItem/NavMenuItemButton";
+
+export const NavMenu: React.FC = ({ }) => {
+
+    const {user, logout} = useAuth();
+
+    return (
+        <div className={styles.container}>
+
+            <div className={styles.userInfo}>
+                <h3 className={styles.username}>{user?.nickname}</h3>
+                <p className={styles.role}>Użytkownik</p>
+            </div>
+
+            <div className={styles.innerContainer}>
+
+                <NavMenuItemLink
+                    location="/studio">
+                    jeteo™ studio
+                </NavMenuItemLink>
+
+                <NavMenuItemLink
+                    location="/options">
+                    ustawienia
+                </NavMenuItemLink>
+
+                <hr className={styles.separator}/>
+
+                {user ? 
+                    <NavMenuItemButton 
+                        onClick={logout}>
+                        wyloguj
+                    </NavMenuItemButton>
+                    :
+                    <NavMenuItemLink
+                        location={'/auth/signin'}>
+                        zaloguj
+                    </NavMenuItemLink>
+                }
+            </div>
+        </div>
+    );
+}
