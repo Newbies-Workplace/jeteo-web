34709b9599317dfd49bc3eb8a1ad2ad4fd00f716
JUtupe <petryszyn@o2.pl>
2022-04-24 20:44:36 +0200

    CR changes

--- src/views/studio/StudioView.tsx
+++ src/views/studio/StudioView.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import {NavBar} from "../../components/ui/NavBar/NavBar";
-import Calendar from "../../assets/images/icons/calendar.svg"
-import Dashboard from "../../assets/images/icons/dashboard.svg"
 import styles from './StudioView.module.scss';
-import {StudioNavButton} from "./StudioNavButton/StudioNavButton";
 import {Route, Routes} from "react-router-dom";
 import {StudioEvents} from "./StudioEvents/StudioEvents";
 import {StudioDashboard} from "./StudioDashboard/StudioDashboard";
 import {RequireAuth} from "../../components/utils/requireAuth/RequireAuth";
+import {StudioNav} from "./StudioNav/StudioNav";
 
 export const StudioView: React.FC = () => {
 
@@ -16,19 +14,7 @@ export const StudioView: React.FC = () => {
             <div className={styles.container}>
                 <NavBar withBackground/>
                 <div className={styles.pageContainer}>
-                    <div className={styles.navigation}>
-                        <h1 className={styles.logo}>STUDIO</h1>
-
-                        <StudioNavButton
-                            icon={<Dashboard width={20}/>}
-                            name={"Dashboard"}
-                            to={"dashboard"}/>
-
-                        <StudioNavButton
-                            icon={<Calendar width={20}/>}
-                            name={"Wydarzenia"}
-                            to={"events"}/>
-                    </div>
+                    <StudioNav/>
 
                     <div className={styles.separator}/>
 
