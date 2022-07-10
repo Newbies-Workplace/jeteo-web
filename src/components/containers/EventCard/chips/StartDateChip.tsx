d0b12685c13895efa86cb3ccd1780abfa3cb5bdc
DiD3n <den190242@gmail.com>
2022-07-10 21:15:00 +0200

    Use dayjs in StartDateChip

--- src/components/containers/EventCard/chips/StartDateChip.tsx
+++ src/components/containers/EventCard/chips/StartDateChip.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import styles from "./chipStyles.module.css";
 import ClockIcon from "../../../../assets/images/icons/clock.svg";
+import dayjs, {ConfigType, Dayjs} from "dayjs";
 
 interface StartDateChipProps {
-    date: Date
+    date: ConfigType | Dayjs
 }
 
-export const StartDateChip: React.FC<StartDateChipProps> = ({date}) => (
-    <div className={styles.chip}>
-        <ClockIcon/>
-        <span className={styles.text}>
-            {date.toDateString()}
-        </span>
-    </div>
-)
+export const StartDateChip: React.FC<StartDateChipProps> = ({date}) => {
+    const startDate = dayjs(date)
+
+    return (
+        <div className={styles.chip}>
+            <ClockIcon/>
+            <span className={styles.text}>
+                {/* display date with year only if year is different for current*/}
+                {startDate.format(`D MMMM ${startDate.year() !== dayjs().year() ? 'YYYY' : ''}, HH:mm`)}
+            </span>
+        </div>
+    )
+}
