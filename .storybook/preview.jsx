98e6414102ddff906ca99a3f5fae75288f08569c
JUtupe <petryszyn@o2.pl>
2022-07-31 13:30:23 +0200

    storybook fix

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -8,4 +8,17 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  backgrounds: {
+    default: 'silver',
+    values: [
+      {
+        name: 'silver',
+        value: '#C0C0C0',
+      },
+      {
+        name: 'white',
+        value: '#ffffff',
+      },
+    ],
+  },
 }
