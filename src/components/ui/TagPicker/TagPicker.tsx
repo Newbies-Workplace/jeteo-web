b2f31bb407f4b09b7de91654e0b5dba4fa1a33b6
JUtupe <petryszyn@o2.pl>
2022-08-03 23:08:53 +0200

    initial tag picker

--- src/components/ui/TagPicker/TagPicker.tsx
+++ src/components/ui/TagPicker/TagPicker.tsx
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
+import React, {createRef, useEffect, useState} from "react";
 
 import styles from "./TagPicker.module.scss"
 import {Tag} from "../../../common/models/Tag";
 import Close from "../../../assets/vectors/close.svg"
+import cs from "classnames";
 
 interface TagPickerProps {
     tags: Tag[]
@@ -19,40 +20,105 @@ interface TagProps {
 const TagComponent: React.FC<TagProps> = ({label, onRemoveClick}) => (
     <div className={styles.tag}>
         {label}
-        <Close className={styles.close} onClick={onRemoveClick}/>
+        <Close className={cs(styles.close, styles.pointer)} onClick={onRemoveClick}/>
+    </div>
+)
+
+interface PickableTagComponentProps {
+    label: string
+    onClick: () => void
+}
+
+const PickableTagComponent: React.FC<PickableTagComponentProps> = ({label, onClick}) => (
+    <div className={styles.dropdownOption} onClick={() => onClick()}>
+        {label}
     </div>
 )
 
 const TagPicker: React.FC<TagPickerProps> = ({tags, value, onChange, onCreate}) => {
-    const [newTagName, setNewTagName] = useState('')
+    const [inputValue, setInputValue] = useState('')
+    const [dropdownVisible, setDropdownVisible] = useState(false)
+    const [dropdownTags, setDropdownTags] = useState<Tag[]>([])
+    const inputRef = createRef<HTMLInputElement>()
+
+    useEffect(() => {
+        const options = tags.filter(t => !value.includes(t))
+            .filter(t => t.name.toLowerCase().startsWith(inputValue.toLowerCase()))
+            .sort((a, b) => a.name.localeCompare(b.name))
+
+        setDropdownTags(options)
+    }, [tags, inputValue, value])
+
+    useEffect(() => {
+        setDropdownVisible(inputValue.length !== 0)
+    }, [inputValue])
 
     const onTagRemoveClick = (tag: Tag) => {
         onChange(value.filter(t => t.id !== tag.id))
     }
 
+    const onTagAddClick = (tag: Tag) => {
+        setDropdownVisible(false)
+        setInputValue('')
+        onChange([...value, tag])
+    }
+
+    const onTagCreateClick = () => {
+        onCreate(inputValue.trim())
+        setInputValue('')
+    }
+
+    const onInputSubmit = () => {
+
+    }
+
     return (
-        <div className={styles.container}>
-            {value.map((tag) => (
-                <TagComponent
-                    key={tag.id}
-                    label={tag.name}
-                    onRemoveClick={() => onTagRemoveClick(tag)}/>
-            ))}
-            <form onSubmit={(e) => {
-                e.preventDefault()
-                setNewTagName('')
-                onCreate(newTagName)
-            }}>
-                <input
-                    className={styles.input}
-                    type={'text'}
-                    value={newTagName}
-                    placeholder={'Wybierz tag...'}
-                    onChange={(e) => {
-                        setNewTagName(e.target.value)
-                    }} />
-            </form>
-        </div>
+        <>
+            <div className={styles.container} onClick={() => inputRef.current?.focus()}>
+                {value.map((tag) => (
+                    <TagComponent
+                        key={tag.id}
+                        label={tag.name}
+                        onRemoveClick={() => onTagRemoveClick(tag)}/>
+                ))}
+
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    onInputSubmit()
+                }}>
+                    <div className={styles.inputContainer}>
+                        <input
+                            ref={inputRef}
+                            className={styles.input}
+                            type={'text'}
+                            value={inputValue}
+                            placeholder={'Wybierz tag...'}
+                            onChange={(e) => setInputValue(e.target.value)} />
+
+                        {inputValue.length !== 0
+                            ? <Close className={cs(styles.close, styles.pointer)} onClick={() => setInputValue('')}/>
+                            : <div className={styles.close}/>
+                        }
+                    </div>
+                </form>
+            </div>
+            {dropdownVisible && (
+                <div className={styles.dropdown}>
+                    {(dropdownTags.length === 0 || (inputValue.length !== 0 && tags.findIndex(t => t.name.toLowerCase() === inputValue.toLowerCase()) === -1)) &&
+                        <PickableTagComponent
+                            key={'add'}
+                            label={"Dodaj: " + inputValue.trim()}
+                            onClick={() => onTagCreateClick()}/>
+                    }
+                    {dropdownTags.map((tag) => (
+                        <PickableTagComponent
+                            key={tag.id}
+                            label={tag.name}
+                            onClick={() => onTagAddClick(tag)}/>
+                    ))}
+                </div>
+            )}
+        </>
     )
 }
 
