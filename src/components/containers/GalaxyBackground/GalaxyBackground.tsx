8611194295a5ca750684dac07c3d9cfbaf517557
JUtupe <petryszyn@o2.pl>
2022-08-09 16:26:13 +0200

    wip

--- src/components/containers/GalaxyBackground/GalaxyBackground.tsx
+++ src/components/containers/GalaxyBackground/GalaxyBackground.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-import styles from "./GalaxyBackground.module.css";
-
-import GalaxyBg from "../../../assets/images/GalaxyBg.png";
+import styles from "./GalaxyBackground.module.scss";
 
 export const GalaxyBackground: React.FC = ({ children }) => {
     return (
-        <div className={styles.wrapper} style={{backgroundImage: `url(${GalaxyBg})`}}>
+        <div className={styles.wrapper}>
+            <div className={styles.stars}/>
+            <div className={styles.stars2}/>
+            <div className={styles.stars3}/>
             {children}
         </div>
     );
