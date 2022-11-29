fa1fe9c4f809f7f1f1fc0bf092c4b12503b30051
Fancy <maciekgplay@gmail.com>
2022-11-29 13:30:41 +0100

    Fixed "404" illustration not showing up
    The "404" illustration was failing to load on the 404 page. The image tag has been replaced with a component.

--- src/views/404/NotFound.tsx
+++ src/views/404/NotFound.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GalaxyBackground from "../../components/containers/GalaxyBackground/GalaxyBackground";
 import { NavBar } from "../../components/ui/NavBar/NavBar";
+import NotFoundImage from "../../assets/images/not-found.svg"
 import Button from "../../components/ui/Button/Button";
 import styles from "./NotFound.module.scss"
 
@@ -11,7 +12,7 @@ export const NotFound: React.FC = () => {
             
             <div className={styles.container}>
                 <div className={styles.innerContainer}>
-                    <img src="../../assets/images/not-found.svg" alt="Błąd 404" />
+                    <NotFoundImage/>
                     <h1>Gdzie ja to miałem?</h1>
                     <p>miejsce do którego zmierzasz już nie istnieje lub nigdy nie istniało</p>
                     <a href="/">
