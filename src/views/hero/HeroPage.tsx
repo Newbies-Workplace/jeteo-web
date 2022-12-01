3dc661fcac307c00ab78b356d21cee572cd03b4b
Fancy <maciekgplay@gmail.com>
2022-12-01 12:29:06 +0100

    Simplified classes & new section
    Co-Authored-By: Eryk <97456294+donocummy@users.noreply.github.com>

--- src/views/hero/HeroPage.tsx
+++ src/views/hero/HeroPage.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import styles from './HeroPage.module.scss'
 import cs from 'classnames'
-//components import
+
+// Components import
+
 import Button from "../../components/ui/Button/Button";
 import GalaxyBackground from "../../components/containers/GalaxyBackground/GalaxyBackground";
 
-//svg import
+// SVG import
+
 import PlanetLogo from "../../assets/images/planet-logo.svg"
 import MoonSeparator from "../../assets/images/moon-decoration.svg"
 import Presentation from "../../assets/images/presentation-svg.svg"
-
+import SpaceWaveBackground from "../../assets/images/hero-bottom-section-bg.svg"
 
 
 export const HeroPage: React.FC = () => {
   return (
-
-    //firts Page
     <div className={styles.mainContainer}>
       <GalaxyBackground>
         <div className={styles.frontPageInfo}>
@@ -31,7 +32,7 @@ export const HeroPage: React.FC = () => {
           </div>
           
           <div className={styles.callToAction}>
-            <h3 className={styles.callToActionText}>Przeglądaj <br/> wydarzenia w okolicy!</h3>
+            <h3>Przeglądaj <br/> wydarzenia w okolicy!</h3>
             <Button className={styles.actionButton}>Lista Wydarzeń</Button>
             <Button className={cs(styles.actionButton, styles.logInButton)}>Zaloguj się</Button>
           </div>
@@ -40,40 +41,69 @@ export const HeroPage: React.FC = () => {
 
 
 
-      {/* Second Page */}
-
-
-
-      <div className={styles.heroSection}>
+      <section className={styles.heroSection}>
 
         <MoonSeparator className={styles.moonSeparator}/>
 
         <div className={styles.heroSectionContentWrapper}>
-
-          <div className={styles.heroFeatContentContainer}>
-            <h2 className={styles.featContentTitle}>Jesteśmy dla</h2>
-            <div className={styles.featContentBox}>
-              <span className={styles.featContentPerson}>Mówcy</span>
-              <span className={styles.featContentPerson}>Słuchacze</span>
-              <span className={styles.featContentPerson}>Organizacje</span>
-            </div>
+          <div className={styles.featContent}>
+            <h2>Jesteśmy dla</h2>
+              <ul className={styles.featContentList}>
+                <li>Mówcy</li>
+                <li>Słuchacze</li>
+                <li>Organizacje</li>
+              </ul>
           </div>
 
-          <div className={styles.heroDownSection}>
+          <div className={styles.heroSectionContent}>
             
-            <div className={styles.heroPresentation}>
+            <div className={styles.heroSectionContentColumn}>
               <Presentation/>
             </div>
-            <div className={styles.heroInfoContainer}>
-              <h3 className={styles.heroInfoTitle}>Czerpnij wiedzę</h3>
-              <p className={styles.description}>Jesteśmy przekonani iż wiedza od ekspertów, jest warta więcej niż jakikolwiek poradnik na youtube. Staramy się stworzyć miejsce z wyłącznie takich ekspertów.</p>
+
+            <div className={styles.heroSectionContentColumn}>
+              <h3>Czerpnij wiedzę</h3>
+              <p>
+                Jesteśmy przekonani iż wiedza od ekspertów, jest warta więcej niż jakikolwiek poradnik na youtube. 
+                Staramy się stworzyć miejsce z wyłącznie takich ekspertów.
+              </p>
             </div>
+
           </div>
         </div>
 
-      </div>
+      </section>
 
 
+      <section className={styles.heroSection}>
+
+        <div className={styles.heroSectionContentWrapper}>
+          <div className={styles.heroSectionContentColumn}>
+            <h3>Dziel się wiedzą</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              Sequi earum necessitatibus porro tenetur sint quia voluptatem placeat assumenda voluptatum. 
+              Dolorem.
+            </p>
+          </div>
+        </div>
+
+      </section>
+
+      {/* <section className={styles.heroSection}>
+
+        <div className={styles.heroSectionContentWrapper}>
+          <div className={styles.heroSectionContentColumn}>
+              <h3>Dziel się wiedzą</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Sequi earum necessitatibus porro tenetur sint quia voluptatem placeat assumenda voluptatum. 
+                Dolorem.
+              </p>
+          </div>
+        </div>
+
+      </section> */}
 
 
     </div>
