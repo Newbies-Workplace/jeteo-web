9b4aa18debe5961587965c756b7b786b33579ebf
Natoprog <nata2004no@gmail.com>
2022-12-05 11:09:40 +0100

    add new star rating component

--- src/components/ui/StarRating/StarRating.tsx
+++ src/components/ui/StarRating/StarRating.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 import styles from "./StarRating.module.scss";
-
-
+import StarIcon from "../../../assets/icons/starIcon.svg";
 interface starRatingProps {
-    starIcon: React.ReactNode,
-    starLenght?: number,
+  starLenght?: number;
+  value: number;
+  setValue: (value: number) => void;
 }
 
-const StarRating: React.FC<starRatingProps> = ({ starIcon, starLenght = 5}) => {
-    return (
-        <p>Witaj</p>
-    )
-}
+
+const StarRating: React.FC<starRatingProps> = ({
+  starLenght = 5,
+  setValue,
+  value,
+}) => {
+  return (
+    <div className={styles.starIconContainer}>
+      {[...Array(starLenght)].map((el, index) => {
+        const fill = index <= value ? "#FFD700" : "transparent";
+        return (
+          <StarIcon
+            className={styles.starIcon}
+            fill={fill}
+            onClick={() => setValue(index)}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default StarRating;
