6b6a13a3a3d3be2148a8680a1f24d571d57c02af
Natoprog <nata2004no@gmail.com>
2022-12-06 10:21:24 +0100

    Star Rating update

--- src/components/ui/StarRating/StarRating.tsx
+++ src/components/ui/StarRating/StarRating.tsx
@@ -1,33 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./StarRating.module.scss";
 import StarIcon from "../../../assets/icons/starIcon.svg";
 interface starRatingProps {
-  starLenght?: number;
+  max?: number;
   value: number;
   setValue: (value: number) => void;
 }
 
-
 const StarRating: React.FC<starRatingProps> = ({
-  starLenght = 5,
+  max = 5,
   setValue,
   value,
 }) => {
+  const [highlited, setHighlited] = useState<number | undefined>();
+
   return (
-    <div className={styles.starIconContainer}>
-      {[...Array(starLenght)].map((el, index) => {
-        const fill = index <= value ? "#FFD700" : "transparent";
+    <div
+      className={styles.starIconContainer}
+      onMouseLeave={() => setHighlited(undefined)}
+    >
+      {[...Array(max)].map((el, index) => {
+        const ok = highlited !== undefined ? highlited : value;
+        const fill = index <= ok ? "#FFD700" : "transparent";
         return (
           <StarIcon
             key={index}
             className={styles.starIcon}
             fill={fill}
             onClick={() => setValue(index)}
+            onMouseEnter={() => setHighlited(index)}
           />
         );
       })}
     </div>
   );
 };
+
 
 export default StarRating;
