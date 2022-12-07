ede413b846a14ff0a54f932f752a8ff517abe3a7
Fancy <maciekgplay@gmail.com>
2022-12-07 13:05:46 +0100

    Change GalaxyBackground

--- src/components/containers/GalaxyBackground/GalaxyBackground.tsx
+++ src/components/containers/GalaxyBackground/GalaxyBackground.tsx
@@ -9,35 +9,71 @@ import Ufo3 from "../../../assets/images/ufo-3.svg"
 import Rocket from "../../../assets/images/rocket.svg" // todo rocket svg
 import PlanetYellow from "../../../assets/images/planet-yellow.svg"
 import SpaceElectricVehicle from "../../../assets/images/chair.svg"
+import { BooleanValueNode } from "graphql";
 
-export const GalaxyBackground: React.FC = ({ children }) => {
+interface GalaxyBackgroundProps {
+    hideStars?: boolean;
+    hideComets?: boolean;
+    hideMainPlanet?: boolean;
+    hideUfoSwarm?: boolean;
+    hideRocket?: boolean;
+    hideVehicle?: boolean;
+}
+
+export const GalaxyBackground: React.FC<GalaxyBackgroundProps> = (
+        { 
+            children,
+            hideStars,
+            hideComets,
+            hideMainPlanet,
+            hideUfoSwarm,
+            hideRocket,
+            hideVehicle
+        }
+    ) => {
     return (
         <div className={styles.wrapper} style={{backgroundImage: `url(${SpaceFog})`, backgroundSize: 'cover'}}>
-            <div className={styles.stars}/>
-            <div className={styles.stars2}/>
-            <div className={styles.stars3}/>
+            {!hideStars &&
+                <>
+                    <div className={styles.stars}/>
+                    <div className={styles.stars2}/>
+                    <div className={styles.stars3}/>
+                </>
+            }
 
-            <div className={styles.comets}>
-                <div className={styles.comet}/>
-                <div className={styles.comet}/>
-                <div className={styles.comet}/>
-            </div>
+            {!hideComets &&
+                <>
+                    <div className={styles.comets}>
+                        <div className={styles.comet}/>
+                        <div className={styles.comet}/>
+                        <div className={styles.comet}/>
+                    </div>
+                </>
+            }
 
-            <PlanetViolet className={styles.planet}/>
-            <div className={styles.planet2Container}>
-                <PlanetYellow className={styles.planet2Moon}/>
-                <PlanetBlue className={styles.planet2}/>
-            </div>
+            {!hideMainPlanet &&
+                <>
+                    <PlanetViolet className={styles.planet}/>
+                        <div className={styles.planet2Container}>
+                        <PlanetYellow className={styles.planet2Moon}/>
+                        <PlanetBlue className={styles.planet2}/>
+                    </div>
+                </>
+            }
 
-            <div className={styles.ufoSwarm}>
-                <Ufo1 className={styles.ufo}/>
-                <Ufo2 className={styles.ufo}/>
-                <Ufo3 className={styles.ufo}/>
-            </div>
+            {!hideUfoSwarm &&
+                <>
+                    <div className={styles.ufoSwarm}>
+                        <Ufo1 className={styles.ufo}/>
+                        <Ufo2 className={styles.ufo}/>
+                        <Ufo3 className={styles.ufo}/>
+                    </div>
+                </>
+            }
 
-            <Rocket className={styles.rocket}/>
+            {!hideRocket && <Rocket className={styles.rocket}/>}
 
-            <SpaceElectricVehicle className={styles.vehicle}/>
+            {!hideVehicle && <SpaceElectricVehicle className={styles.vehicle}/>}
 
             {children}
         </div>
