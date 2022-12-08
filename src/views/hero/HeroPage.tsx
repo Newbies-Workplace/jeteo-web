c12288c5365bec210921491576a9574172b393c6
Fancy <maciekgplay@gmail.com>
2022-12-08 12:56:02 +0100

    Responsiveness adjustments
    Co-Authored-By: Eryk <97456294+donocummy@users.noreply.github.com>

--- src/views/hero/HeroPage.tsx
+++ src/views/hero/HeroPage.tsx
@@ -2,18 +2,12 @@ import React from "react";
 import styles from './HeroPage.module.scss'
 import cs from 'classnames'
 
-// Components import
-
 import Button from "../../components/ui/Button/Button";
 import GalaxyBackground from "../../components/containers/GalaxyBackground/GalaxyBackground";
-
-// SVG import
 
 import PlanetLogo from "../../assets/images/planet-logo.svg"
 import MoonSeparator from "../../assets/images/moon-decoration.svg"
 import Presentation from "../../assets/images/presentation-svg.svg"
-import SpaceWaveBackground from "../../assets/images/hero-bottom-section-bg.svg"
-
 
 export const HeroPage: React.FC = () => {
   return (
@@ -81,34 +75,19 @@ export const HeroPage: React.FC = () => {
             <div className={styles.heroSectionContentBox}>
                 <h3>Dziel się wiedzą</h3>
                 <p>
+                Masz ego nie z tego świata i potrzebę wygadania się? To miejsce stworzone dla ciebie! Jedynie co potrzebujesz to bycie ekspertem i potężnego pytonga.
+                </p>
+                {/* <h3>Dziel się wiedzą</h3>
+                <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                   Nesciunt fugiat, dolorum excepturi natus explicabo quidem ipsum omnis suscipit iste obcaecati.
-                </p>
+                </p> */}
             </div>
           </div>
         </div>
 
       </section>
 
-      {/* <section className={styles.heroSection}>
-
-        <div className={styles.heroSectionContentContainer}>
-          <div className={styles.heroSectionContent}>
-            <div className={styles.heroSectionContentBox}>
-              <h3>Trzecia sekcja</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Consectetur voluptatibus recusandae culpa autem eligendi sed similique nemo, 
-                distinctio assumenda ipsa excepturi voluptas veniam nesciunt quidem consequatur totam minus repudiandae! Iure?
-              </p>
-            </div>
-          </div>
-        </div>
-
-      </section> */}
-
-
     </div>
-    
   );
 };
