9273605f8470b2f32d188c06e689b3e6fb0fcee1
Fancy <maciekgplay@gmail.com>
2022-12-09 13:06:40 +0100

    Add Big Map Popup
    The user can now open a way bigger map by clicking a button in the smaller map, or by double clicking it. The bigger map closes with the same button seen in the small map and by clicking outside of it.

--- src/components/ui/LocationMap/LocationMap.tsx
+++ src/components/ui/LocationMap/LocationMap.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from "leaflet";
 import styles from "./LocationMap.module.scss";
+import ZoomIcon from "../../../assets/icons/zoom-glass.svg";
 
 interface LocationMapProps {
     latitude: number,
@@ -16,6 +17,14 @@ const markerIcon = new L.Icon({
 })
 
 export const LocationMap: React.FC<LocationMapProps> = ({latitude, longitude, address}) => {
+
+    const [isBigMapToggled, toggleBigMap] = useState<boolean>(false)
+
+    /*
+        "mapContainerOverlay" classes control toggling with double click
+        "mapBigDisplayOverlay" controls closing the map by clicking outside of it
+    */
+
     return (
         <div className={styles.mapCard}>
             <span className={styles.mapCardTitle}>Lokacja</span>
@@ -26,18 +35,65 @@ export const LocationMap: React.FC<LocationMapProps> = ({latitude, longitude, ad
                 lng: longitude
             }} zoom={20} zoomControl={false} scrollWheelZoom>
 
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+                <div 
+                    className={styles.mapContainerOverlay}
+                    onDoubleClick={() => toggleBigMap(!isBigMapToggled)}>
+                </div>
 
-            <Marker position={[latitude, longitude]} icon={markerIcon}>
-                <Popup>
-                    Lokalizacja prelekcji
-                </Popup>
-            </Marker>
+                <button className={styles.zoomButton} onClick={() => toggleBigMap(!isBigMapToggled)}>
+                    <ZoomIcon width={16} height={16}/>
+                </button>
+
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+
+                <Marker position={[latitude, longitude]} icon={markerIcon}>
+                    <Popup>
+                        Lokalizacja prelekcji
+                    </Popup>
+                </Marker>
 
             </MapContainer>
+
+            {isBigMapToggled &&
+                <>
+                    <div 
+                        className={styles.mapBigDisplayOverlay} 
+                        onClick={() => toggleBigMap(!isBigMapToggled)}
+                    />
+
+                    <div className={styles.mapBigDisplay} style={{display: "block"}}>
+
+                        <MapContainer style={{width: "100%", height: "100%"}} center={{
+                            lat: latitude,
+                            lng: longitude
+                        }} zoom={20} zoomControl={false} scrollWheelZoom>
+
+                            <div    
+                                className={styles.mapContainerOverlay}
+                                onDoubleClick={() => toggleBigMap(!isBigMapToggled)}
+                            />
+
+                            <button className={styles.zoomButton} onClick={() => toggleBigMap(!isBigMapToggled)}>
+                                <ZoomIcon width={16} height={16}/>
+                            </button>
+        
+                            <TileLayer
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+        
+                            <Marker position={[latitude, longitude]} icon={markerIcon}>
+                                <Popup>
+                                    Lokalizacja prelekcji
+                                </Popup>
+                            </Marker>
+        
+                        </MapContainer>
+        
+                    </div>
+                </>
+            }
         </div>
     )
 }
