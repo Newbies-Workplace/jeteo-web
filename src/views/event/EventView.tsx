42d1c186955eebee02b760e2949f9cb0ec8e0b9b
Natoprog <nata2004no@gmail.com>
2022-12-12 11:50:43 +0100

    change Event view with better style

--- src/views/event/EventView.tsx
+++ src/views/event/EventView.tsx
@@ -47,21 +47,21 @@ export const EventView: React.FC = () => {
 
     console.log(lectures);
     
-    const lecturesList = lectures.map(item => (
-        <>
-        <p className={styles.agendaTimeStick}>|</p>
-        <p className={styles.agendaTime}>{dayjs(item.timeFrame.startDate).format('HH:mm')}</p>
-        <p className={styles.agendaTimeStick}>|</p>
-        <Lecture key={item.id} title={item.title} description={item.description} speaker={{
-            name: item.author.nickname, 
-            contact: {
-                githubLink: item.author.contact.github,
-                twitterLink: item.author.contact.twitter,
-                emailLink: item.author.contact.mail,
-                linkedInLink: item.author.contact.linkedin
-        }}} 
-        status={{color: "black" , content: <button>Oceń</button>}} /> 
-        </>
+    const lecturesList = lectures.map((item, index) => (
+        <div key={item.id}>
+            {index !== 0 && <p className={styles.agendaTimeStickTop}>|</p>}
+            <p className={styles.agendaTime}>{dayjs(item.timeFrame.startDate).format('HH:mm')}</p>
+            <p className={styles.agendaTimeStickBottom}>|</p>
+            <Lecture title={item.title} description={item.description} speaker={{
+                name: item.author.nickname, 
+                contact: {
+                    githubLink: item.author.contact.github,
+                    twitterLink: item.author.contact.twitter,
+                    emailLink: item.author.contact.mail,
+                    linkedInLink: item.author.contact.linkedin
+            }}} 
+            status={{color: "black" , content: <button>Oceń</button>}} /> 
+        </div>
     ))
 
     const everyHours = lectures.map(element => (
@@ -88,7 +88,7 @@ export const EventView: React.FC = () => {
 
                     <div className={styles.eventInnerContainer}>
 
-                    <div>
+                    <div className={styles.eventDescriptionContainer}>
                         <EventDescriptionSection
                             description={event.description || ""}/>
                         <p className={styles.agenda}>Agenda</p>
@@ -98,7 +98,7 @@ export const EventView: React.FC = () => {
                     <section className={styles.eventOrganizerSection}>
                         <EventOrganizer logo="" name="Team Jeteo" bio="Super ekstra mega omega giga okropny squad do pisania aplikacji webowych. Z brakiem doświadczenia, przepisujących projekt z php pod nową nazwą." links={{}}/>
                         <p className={styles.eventLinksText}>Linki wydarzenia</p>
-                        <EventLink url='#' name='#'/>
+                        <EventLink url='#' name='#' />
                         <LocationMap latitude={51.085670625464104} longitude={17.010400182993322} address="RST Software Masters"/>
                     </section>
 
