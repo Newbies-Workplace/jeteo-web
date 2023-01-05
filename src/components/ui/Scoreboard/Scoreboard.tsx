4699b89861b76401b31d30ea87f3b29f38445709
JUtupe <petryszyn@o2.pl>
2023-01-05 20:23:27 +0100

    scoreboard styles cleanup

--- src/components/ui/Scoreboard/Scoreboard.tsx
+++ src/components/ui/Scoreboard/Scoreboard.tsx
@@ -20,27 +20,26 @@ export const Scoreboard: React.FC<ScoreboardProps> = ({userPoints}) => {
                 return (
                     <div
                         key={value.name}
-                         className={cs(styles.userPoints, {
-                             [styles.first]: index === 0,
-                             [styles.second]: index === 1,
-                             [styles.third]: index === 2,
-                         })}>
+                        className={cs(styles.userPoints, {
+                            [styles.first]: index === 0,
+                            [styles.second]: index === 1,
+                            [styles.third]: index === 2,
+                        })}>
 
-                        <span className={styles.name}>
-                            {index <= 2 && (
+                        <div className={styles.name}>
+                            {index <= 2 &&
                                 <div className={cs(styles.dot, {
                                     [styles.first]: index === 0,
                                     [styles.second]: index === 1,
                                     [styles.third]: index === 2,
-                                })}/>
-                            )}
+                                })}/>}
 
                             {value.name}
-                        </span>
+                        </div>
 
-                        <span>
+                        <div className={styles.value}>
                             {value.points} pkt.
-                        </span>
+                        </div>
                     </div>
                 )
             })}
